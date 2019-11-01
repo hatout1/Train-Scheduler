3dea6e3b707ab64821e7573308c1ae7f177d01ed
hatout01 <hatout1@gmail.com>
2019-10-31 20:33:53 -0700

    fixed function that sends data to firebase and added another that brings back that data and append it to the webpage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,44 +10,86 @@ $(document).ready(function () {
         messagingSenderId: "368512686890",
         appId: "1:368512686890:web:f89a069701cfe8db085c53"
     };
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
+    // const database as small name for easy use.
     const db = firebase.database()
 
     // arrays to hold admin entries
-    var trainName = [];
-    var destination = [];
-    var firstTrainTime = [];
-    var frequency = [];
+    let trainNameArray = [];
+    let destinationArray = [];
+    let firstTrainTimeArray = [];
+    let frequencyArray = [];
 
-    // variable to help with entries order
-    var ordernumber = 0;
 
     // function that collect data on click submit btn and assign it to arrays
     $('.btn').on('click', function (event) {
         event.preventDefault();
 
-        // increment ordernumber when the buton clicked
-        ordernumber += 1;
-        console.log(ordernumber)
+        // make value as a const to push them after to arrays
+        const trainName = $('#trainName').val();
+        const distination = $('#destination').val();
+        const firstTrain = $('#firstTrainTime').val();
+        const frequency = $('#frequency').val();
+
+        // puch values to the arrays
+        trainNameArray.push(trainName);
+        destinationArray.push(distination)
+        firstTrainTimeArray.push(firstTrain)
+        frequencyArray.push(frequency)
 
         // function that creates and add data to firebase.
-        db.ref(ordernumber).set({
-            ordernumber: ordernumber,
-            trainName: $('#trainName').val(),
-            destination: $('#destination').val(),
-            firstTrainTime: $('#firstTrainTime').val(),
-            frequency: $('#frequency').val(),
+        db.ref('trainschedule').set({
+
+            train: trainNameArray,
+            trainDistination: destinationArray,
+            trainFirstTrain: firstTrainTimeArray,
+            trainFrequency: frequencyArray,
         });
 
-        // console.log(trainName);
+        // to clear input area after submition of entries
+        $('#trainName').val('');
+        $('#destination').val('');
+        $('#firstTrainTime').val('');
+        $('#frequency').val('');
+    });
 
-        db.ref(ordernumber).on('value', snap => {
-            console.log(snap.val());
 
-        })
+    //  function to bring back the data from firebase
+    db.ref('trainschedule').on('value', snap => {
+        trainNameArray = snap.val().train
+        destinationArray = snap.val().train
+        firstTrainTimeArray = snap.val().train
+        frequencyArray = snap.val().train
+
+        // function to avoid multi entries of data.
+        $('#trainName').empty();
+        $('#destination').empty();
+        $('#firstTrainTime').empty();
+        $('#frequency').empty();
+
+        // function to append the values to the webpage
+        trainNameArray.map((train) => {
+            $('#displayName').append(`<tr><td>${train}</td></tr>`)
+        });
+
+        destinationArray.map((trainDistination) => {
+            $('#displayDestination').append(`<tr><td>${trainDistination}</td></tr>`)
+        });
+
+        frequencyArray.map((trainFrequency) => {
+            $('#displayFrequency').append(`<tr><td>${trainFrequency}</td></tr>`)
+        });
+
 
     });
+
+
+    // still need to work on function that calculates "Next arrival train 
+    // & another that calculate "minutes away"
+
+
 
 });
